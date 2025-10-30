416959508eafc63ce107f0d55a5f613abc7131d1
Archit Pandya <architpandya@yahoo.com>
2025-10-30 13:01:06 -0400

    Added: requirements.txt, setup.py, src

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    This function returns requirements
+
+    Args:
+        file_path (str): _description_
+
+    Returns:
+        List[str]: _description_
+    """
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Archit Pandya',
+    author_email='architpandya@yahoo.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
